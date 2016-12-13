61d2352040dbf54a9598abdcdc92ae3f6afdd0a7
See.Ku <info@see-ku.com>
2016-12-13 09:35:47 +0900

    Initial Commit

---
+++ SK4Library/SK4Library.h
@@ -0,0 +1,19 @@
+//
+//  SK4Library.h
+//  SK4Library
+//
+//  Created by See.Ku on 2016/12/13.
+//  Copyright (c) 2016 AxeRoad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SK4Library.
+FOUNDATION_EXPORT double SK4LibraryVersionNumber;
+
+//! Project version string for SK4Library.
+FOUNDATION_EXPORT const unsigned char SK4LibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SK4Library/PublicHeader.h>
+
+
